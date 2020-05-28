76c552544df00e887670ca1cbf50ea9e0d3f66c1
ablyabhishek <abhishek.rana@fatbit.in>
2020-05-28 15:19:43 +0530

    bug-036722-calendar-and-list-button

--- application/views/learner-scheduled-lessons/index.php
+++ application/views/learner-scheduled-lessons/index.php
@@ -1,57 +1,57 @@
 <?php defined('SYSTEM_INIT') or die('Invalid Usage.'); ?>
 <section class="section section--grey section--page">
 	 <div class="container container--fixed">
-	   
+
 	   <div class="page-panel -clearfix">
-	   
+
 			<!--panel left start here-->
 			<div class="page-panel__left">
 				<?php $this->includeTemplate('account/_partial/dashboardNavigation.php'); ?>
 			</div>
             <!--panel left end here-->
 
-	   
+
 			<!--panel right start here-->
 		   <div class="page-panel__right">
-				
+
 				 <!--page-head start here-->
 				 <div class="page-head">
 				   <div class="d-flex justify-content-between align-items-center">
 						 <div><h1><?php echo Label::getLabel('LBL_My_Lessons'); ?></h1></div>
 						 <div>
-							 <div class="tab-swticher tab-swticher-small">
-								<a href="<?php echo CommonHelper::generateUrl('LearnerScheduledLessons'); ?>" class="btn btn--large is-active"><?php echo Label::getLabel('LBL_List'); ?></a>
-								<a onclick="viewCalendar();" href="javascript:void(0);" class="btn btn--large"><?php echo Label::getLabel('LBL_Calender'); ?></a>
+							 <div class="tab-swticher tab-swticher-small calender-lessons-js">
+								<a href="<?php echo CommonHelper::generateUrl('LearnerScheduledLessons'); ?>" class="btn btn--large is-active list-js"><?php echo Label::getLabel('LBL_List'); ?></a>
+								<a onclick="viewCalendar();" href="javascript:void(0);" class="btn btn--large calender-js"><?php echo Label::getLabel('LBL_Calender'); ?></a>
 							 </div>
-								 
+
 						 </div>
 					 </div>
 				 </div>
 				 <!--page-head end here-->
-				 
+
 				 <!--page filters start here-->
 				 <div class="page-filters">
-					<?php 
+					<?php
 					$frmSrch->setFormTagAttribute ( 'onsubmit', 'searchLessons(this); return(false);');
 					$frmSrch->setFormTagAttribute ( 'class', 'form form--small' );
-					
+
 					$frmSrch->developerTags['colClassPrefix'] = 'col-md-';
 					$frmSrch->developerTags['fld_default_col'] = 5;
-					
+
 					$fldStatus = $frmSrch->getField( 'status' );
 					$fldStatus->developerTags['col'] = 3;
-					
+
 					$fldSubmit = $frmSrch->getField( 'btn_submit' );
 					$fldSubmit->developerTags['col'] = 4;
-					
+
 					$btnReset = $frmSrch->getField( 'btn_reset' );
 					//$btnReset->addFieldTagAttribute( 'style', 'margin-left:10px' );
 					$btnReset->addFieldTagAttribute('onclick','clearSearch()');
 					echo $frmSrch->getFormHtml(); ?>
-				 
+
 				 </div>
 				 <!--page filters end here-->
-			   
+
 				<!--Lessons list view start here-->
 				<div class="col-list-group">
 					<!--h6>Today</h6-->
--- application/views/learner-scheduled-lessons/page-js/index.js
+++ application/views/learner-scheduled-lessons/page-js/index.js
@@ -1,146 +1,148 @@
 $(function() {
 	var dv = '#listItemsLessons';
 	searchLessons = function(frm){
+		$('.calender-lessons-js a').removeClass('is-active');
+		$('.list-js').addClass('is-active');
 		$(dv).html(fcom.getLoader());
 		var data = fcom.frmData(frm);
 		fcom.ajax(fcom.makeUrl('LearnerScheduledLessons','search'),data,function(t){
 			$(dv).html(t);
 		});
 	};
-	
-	getListingLessonPlans = function(id){	
+
+	getListingLessonPlans = function(id){
 		$(dv).html(fcom.getLoader());
 		fcom.ajax(fcom.makeUrl('LearnerScheduledLessons','getListingLessonPlans',[id]),'',function(t){
 			searchLessons(document.frmSrch);
 			$.facebox( t,'facebox-medium');
-		}); 
+		});
 	};
-	
-	changeLessonPlan = function(id){	
+
+	changeLessonPlan = function(id){
 		$(dv).html(fcom.getLoader());
 		fcom.ajax(fcom.makeUrl('LearnerScheduledLessons','changeLessonPlan',[id]),'',function(t){
 			searchLessons(document.frmSrch);
 			$.facebox( t,'facebox-medium');
-		}); 
+		});
 	};
-	
-	remove = function(elem,id){	
+
+	remove = function(elem,id){
 		if(confirm(langLbl.confirmRemove))
 		{
 			$(elem).closest('tr').remove();
 			$(dv).html(fcom.getLoader());
 			fcom.ajax(fcom.makeUrl('TeacherLessonsPlan','remove',[id]),'',function(t){
 				searchLessons(document.frmSrch);
-			}); 
+			});
 		}
 	};
-	
+
 	/*setUpLessonSchedule = function(lessonId,startTime,endTime,date){
 		fcom.updateWithAjax(fcom.makeUrl('LearnerScheduledLessons', 'setUpLessonSchedule'), 'lessonId='+lessonId+'&startTime='+startTime+'&endTime='+endTime+'&date='+date, function(doc) {
-			$.facebox.close();				
+			$.facebox.close();
 			searchLessons(document.frmSrch);
 		});
 	}*/
-	
+
 	assignLessonPlanToLessons = function(lessonId,planId){
-		fcom.updateWithAjax(fcom.makeUrl('LearnerScheduledLessons', 'assignLessonPlanToLessons'), 'ltp_slessonid='+lessonId+'&ltp_tlpn_id='+planId , function(t) {		
-				$.facebox.close();				
-				searchLessons(document.frmSrch);	
-		});	
+		fcom.updateWithAjax(fcom.makeUrl('LearnerScheduledLessons', 'assignLessonPlanToLessons'), 'ltp_slessonid='+lessonId+'&ltp_tlpn_id='+planId , function(t) {
+				$.facebox.close();
+				searchLessons(document.frmSrch);
+		});
 	};
-	
+
 	removeAssignedLessonPlan = function(lessonId){
 		if(confirm(langLbl.confirmRemove))
 		{
-			fcom.updateWithAjax(fcom.makeUrl('LearnerScheduledLessons', 'removeAssignedLessonPlan'), 'ltp_slessonid='+lessonId , function(t) {		
-					$.facebox.close();				
+			fcom.updateWithAjax(fcom.makeUrl('LearnerScheduledLessons', 'removeAssignedLessonPlan'), 'ltp_slessonid='+lessonId , function(t) {
+					$.facebox.close();
 					searchLessons(document.frmSrch);
-			});	
+			});
 		}
 	};
-	
-	viewAssignedLessonPlan = function(lessonId){	
-		fcom.ajax(fcom.makeUrl('LearnerScheduledLessons', 'viewAssignedLessonPlan',[lessonId]), '', function(t) {		
+
+	viewAssignedLessonPlan = function(lessonId){
+		fcom.ajax(fcom.makeUrl('LearnerScheduledLessons', 'viewAssignedLessonPlan',[lessonId]), '', function(t) {
 				searchLessons(document.frmSrch);
 			$.facebox( t,'facebox-medium');
-		});	
+		});
 	};
-	
-	/*viewBookingCalendar = function(id){	
+
+	/*viewBookingCalendar = function(id){
 		fcom.ajax(fcom.makeUrl('LearnerScheduledLessons','viewBookingCalendar',[id]),'',function(t){
 			//searchLessons(document.frmSrch);
 			$.facebox( t,'facebox-medium');
 		});
 	};*/
-	
-	viewCalendar = function(frm){	
+
+	viewCalendar = function(frm){
 		var data = fcom.frmData(frm);
 		fcom.ajax(fcom.makeUrl('LearnerScheduledLessons','viewCalendar'),data,function(t){
 			$(dv).html(t);
 		});
 	};
-	
-	/*cancelLesson = function(id){	
+
+	/*cancelLesson = function(id){
 		$(dv).html(fcom.getLoader());
 		fcom.ajax(fcom.makeUrl('LearnerScheduledLessons','cancelLesson',[id]),'',function(t){
 			searchLessons(document.frmSrch);
 			$.facebox( t,'facebox-medium');
-		}); 
+		});
 	};
-	
-	cancelLessonSetup = function(frm){	
-	if (!$(frm).validate()) return;	
+
+	cancelLessonSetup = function(frm){
+	if (!$(frm).validate()) return;
 		var data = fcom.frmData(frm);
-		fcom.updateWithAjax(fcom.makeUrl('LearnerScheduledLessons', 'cancelLessonSetup'), data , function(t) {		
-				$.facebox.close();				
+		fcom.updateWithAjax(fcom.makeUrl('LearnerScheduledLessons', 'cancelLessonSetup'), data , function(t) {
+				$.facebox.close();
 				searchLessons(document.frmSrch);
-		});	
+		});
 	};*/
-		
-	/*issueReported = function(id){	
+
+	/*issueReported = function(id){
 		$(dv).html(fcom.getLoader());
 		fcom.ajax(fcom.makeUrl('LearnerScheduledLessons','issueReported',[id]),'',function(t){
 			searchLessons(document.frmSrch);
 			$.facebox( t,'facebox-medium');
-		}); 
+		});
 	};
-	
-	issueReportedSetup = function(frm){	
-	if (!$(frm).validate()) return;	
+
+	issueReportedSetup = function(frm){
+	if (!$(frm).validate()) return;
 		var data = fcom.frmData(frm);
-		fcom.updateWithAjax(fcom.makeUrl('LearnerScheduledLessons', 'issueReportedSetup'), data , function(t) {		
-				$.facebox.close();				
-				searchLessons(document.frmSrch);	
-		});	
+		fcom.updateWithAjax(fcom.makeUrl('LearnerScheduledLessons', 'issueReportedSetup'), data , function(t) {
+				$.facebox.close();
+				searchLessons(document.frmSrch);
+		});
 	};*/
-	
-	/*requestReschedule = function(id){	
+
+	/*requestReschedule = function(id){
 		$(dv).html(fcom.getLoader());
 		fcom.ajax(fcom.makeUrl('LearnerScheduledLessons','requestReschedule',[id]),'',function(t){
 			searchLessons(document.frmSrch);
 			$.facebox( t,'facebox-medium');
-		}); 
+		});
 	};
-	
-	requestRescheduleSetup = function(frm){	
-	if (!$(frm).validate()) return;	
+
+	requestRescheduleSetup = function(frm){
+	if (!$(frm).validate()) return;
 		var data = fcom.frmData(frm);
-		fcom.updateWithAjax(fcom.makeUrl('LearnerScheduledLessons', 'requestRescheduleSetup'), data , function(t) {		
-				$.facebox.close();				
+		fcom.updateWithAjax(fcom.makeUrl('LearnerScheduledLessons', 'requestRescheduleSetup'), data , function(t) {
+				$.facebox.close();
 				searchLessons(document.frmSrch);
-		});	
+		});
 	};*/
-	
+
 	clearSearch = function(){
 		document.frmSrch.reset();
 		searchLessons( document.frmSrch );
 	};
-	
+
 	goToSearchPage = function(page) {
 		if(typeof page == undefined || page == null){
 			page = 1;
-		}		
-		var frm = document.frmSLnsSearchPaging;		
+		}
+		var frm = document.frmSLnsSearchPaging;
 		$(frm.page).val(page);
 		searchLessons(frm);
 	};
@@ -149,6 +151,6 @@ $(function() {
 
 
 $(document).on('click','.tab-swticher a',function(){
-	$('.tab-swticher a').removeClass('is-active');
+	$('.calender-lessons-js a').removeClass('is-active');
 	$(this).addClass('is-active');
 });
