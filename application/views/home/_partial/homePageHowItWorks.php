4d3a854c8237982eb981b1ee76a455a45c3dd18c
ablyabhishek <abhishek.rana@fatbit.in>
2020-05-29 20:15:33 +0530

    bug-037605-banner-caption-button

--- application/views/home/_partial/homePageHowItWorks.php
+++ application/views/home/_partial/homePageHowItWorks.php
@@ -1,12 +1,12 @@
 <?php defined('SYSTEM_INIT') or die('Invalid Usage.'); ?>
-<?php 
+<?php
 $hWorksdata = isset($banners['BLOCK_HOW_IT_WORKS'])?$banners['BLOCK_HOW_IT_WORKS']:'';
 $secondBlockdata = isset($banners['BLOCK_SECOND_AFTER_HOMESLIDER'])?$banners['BLOCK_SECOND_AFTER_HOMESLIDER']:'';
 $firstBlockdata = isset($banners['BLOCK_FIRST_AFTER_HOMESLIDER'])?$banners['BLOCK_FIRST_AFTER_HOMESLIDER']:''; ?>
 <?php /* if(!empty($firstBlockdata['banners'])){ //print_r($secondBlockdata); die;?>
 <section class="section section--content">
 	<div class="container container--narrow">
-                        <?php  $i = 1; foreach($firstBlockdata['banners'] as $banners) { 
+                        <?php  $i = 1; foreach($firstBlockdata['banners'] as $banners) {
                         $cls = ($i%2==1)?'order-md-1':'';
                         ?>
 		<div class="row row--custom justify-content-between">
@@ -55,7 +55,7 @@ $bannerDet = current($secondBlockdata['banners']); ?>
                 <h2><?php echo Label::getLabel('LBL_Why_Us?'); ?></h2>
             </div>
             <div class="row justify-content-center align-items-center scroller--horizontal ">
-                    <?php  $i = 1; foreach($secondBlockdata['banners'] as $banners) { ?>            
+                    <?php  $i = 1; foreach($secondBlockdata['banners'] as $banners) { ?>
                 <div class="col-md-4 col-xl-4">
                     <div class="block align-center">
                         <div class="block__media">
@@ -64,6 +64,14 @@ $bannerDet = current($secondBlockdata['banners']); ?>
                         <div class="block__content">
                             <h6><?php echo $banners['banner_title']; ?></h6>
                             <p><?php echo $banners['banner_description']; ?></p>
+                            <?php
+                            $bannerBtnCaption =  (!empty($banners['banner_btn_caption']))  ? $banners['banner_btn_caption'] : Label::getLabel('LBL_View');
+                                if(!empty($banners['banner_btn_url'])) {
+                                ?>
+                                    <a href="<?php echo $banners['banner_btn_url']; ?>" class="btn btn--primary banner-btn-url"><?php echo $bannerBtnCaption; ?></a>
+                                <?php
+                                }
+                            ?>
                         </div>
                     </div>
                 </div>
@@ -99,19 +107,19 @@ $bannerDet = current($secondBlockdata['banners']); ?>
 							</li>
                     <?php $i++; } ?>
 						</ul>
-                    
+
 					</div>
 				</div>
-				
+
 				<div class="col-xl-7 col-lg-7 col-md-12  col-sm-12 col__content">
-                    <?php  $i = 1; foreach($hWorksdata['banners'] as $banners) { ?>                
+                    <?php  $i = 1; foreach($hWorksdata['banners'] as $banners) { ?>
 					<div id="tab<?php echo $i; ?>" class="tabs-content-js">
 						<div class="media"><a href="<?php echo $banners['banner_url']; ?>" target="<?php echo $banners['banner_target']; ?>" ><img src="<?php echo CommonHelper::generateUrl('Image','showBanner',array($banners['banner_id'], 0, BannerLocation::BLOCK_HOW_IT_WORKS)); ?>" alt=""></a></div>
 					</div>
-                    <?php $i++; } ?>                    
+                    <?php $i++; } ?>
 				</div>
 			</div>
-            <?php } ?>        
+            <?php } ?>
 
 	</div>
 </section>
