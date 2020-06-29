74cc13c2faf488d19947e5e5bbb8628b9ed79bec
ablyabhishek <abhishek.rana@fatbit.in>
2020-06-29 14:49:28 +0530

    bug-038999-add-check-on-active-and-inactive-toggel-btn

--- admin-application/views/admin-users/search.php
+++ admin-application/views/admin-users/search.php
@@ -3,9 +3,9 @@
 $arr_flds = array(
 		'listserial'=>Label::getLabel('LBL_Sr._No',$adminLangId),
 		'admin_name'=>Label::getLabel('LBL_Full_Name',$adminLangId),
-		'admin_username'=>Label::getLabel('LBL_Username',$adminLangId),	
-		'admin_email'=>Label::getLabel('LBL_Email',$adminLangId),	
-		'admin_active'=>Label::getLabel('LBL_Status',$adminLangId),	
+		'admin_username'=>Label::getLabel('LBL_Username',$adminLangId),
+		'admin_email'=>Label::getLabel('LBL_Email',$adminLangId),
+		'admin_active'=>Label::getLabel('LBL_Status',$adminLangId),
 		'action' => Label::getLabel('LBL_Action',$adminLangId),
 	);
 $tbl = new HtmlElement('table', array('width'=>'100%', 'class'=>'table table-responsive'));
@@ -18,7 +18,7 @@ $sr_no = 0;
 foreach ($arr_listing as $sn=>$row){
 	$sr_no++;
 	$tr = $tbl->appendElement('tr');
-	
+
 	foreach ($arr_flds as $key=>$val){
 		$td = $tr->appendElement('td');
 		switch ($key){
@@ -28,33 +28,34 @@ foreach ($arr_listing as $sn=>$row){
 			case 'action':
 				$ul = $td->appendElement("ul",array("class"=>"actions actions--centered"));
 				if($canEdit){
-					$li = $ul->appendElement("li",array('class'=>'droplink'));	
-					
+					$li = $ul->appendElement("li",array('class'=>'droplink'));
+
 					if($row['admin_id'] > 1 || $adminLoggedInId==1)
 					{
 						$li->appendElement('a', array('href'=>'javascript:void(0)', 'class'=>'button small green','title'=>Label::getLabel('LBL_Edit',$adminLangId)),'<i class="ion-android-more-horizontal icon"></i>', true);
 					}
-					$innerDiv=$li->appendElement('div',array('class'=>'dropwrap'));	
+					$innerDiv=$li->appendElement('div',array('class'=>'dropwrap'));
 					$innerUl=$innerDiv->appendElement('ul',array('class'=>'linksvertical'));
-              		
+
 					$innerLi=$innerUl->appendElement('li');
 					$innerLi->appendElement('a', array('href'=>'javascript:void(0)','class'=>'button small green','title'=>Label::getLabel('LBL_Edit',$adminLangId),"onclick"=>"editAdminUserForm(".$row['admin_id'].")"),Label::getLabel('LBL_Edit',$adminLangId), true);
-					
+
 					$innerLi=$innerUl->appendElement('li');
-					$innerLi->appendElement('a', array('href'=>'javascript:void(0)','class'=>'button small green','title'=>Label::getLabel('LBL_Change_Password',$adminLangId),"onclick"=>"changePasswordForm(".$row['admin_id'].")"),Label::getLabel('LBL_Change_Password',$adminLangId), true);					
-					
-					
-					if($row['admin_id'] > 1 && $row['admin_id']!=$adminLoggedInId){
-						
+					$innerLi->appendElement('a', array('href'=>'javascript:void(0)','class'=>'button small green','title'=>Label::getLabel('LBL_Change_Password',$adminLangId),"onclick"=>"changePasswordForm(".$row['admin_id'].")"),Label::getLabel('LBL_Change_Password',$adminLangId), true);
+
+
+					if($row['admin_id'] > 1 && $row['admin_id'] != $adminLoggedInId){
+
 						$innerLi=$innerUl->appendElement('li');
-						$innerLi->appendElement('a', array('href'=>CommonHelper::generateUrl('AdminUsers', 'permissions', array($row['admin_id'])),'class'=>'button small green redirect--js','title'=>Label::getLabel('LBL_Permissions',$adminLangId)),Label::getLabel('LBL_Permissions',$adminLangId), true);	
-					
+						$innerLi->appendElement('a', array('href'=>CommonHelper::generateUrl('AdminUsers', 'permissions', array($row['admin_id'])),'class'=>'button small green redirect--js','title'=>Label::getLabel('LBL_Permissions',$adminLangId)),Label::getLabel('LBL_Permissions',$adminLangId), true);
+
 					}
-					
-					
+
+
 				}
 			break;
 			case 'admin_active':
+			if ($row['admin_id'] > 1) {
 					$active = "active";
 					$statucAct='';
 					if($row['admin_active']==applicationConstants::YES &&  $canEdit === true ) {
@@ -70,6 +71,7 @@ foreach ($arr_listing as $sn=>$row){
 					  <span class="switch-handles"></span>
 					</label>';
 					$td->appendElement('plaintext', array(), $str,true);
+				}
 			break;
 			default:
 				$td->appendElement('plaintext', array(), $row[$key],true);
