66edfd142817c23e801f7c92c5bb8b7b2cf9a509
ablyabhishek <abhishek.rana@fatbit.in>
2020-07-22 19:09:51 +0530

    bug-039935-Changed-Dimensions

--- admin-application/views/slides/media-form.php
+++ admin-application/views/slides/media-form.php
@@ -3,31 +3,31 @@ $slideMediaFrm->setFormTagAttribute('class', 'web_form form_horizontal');
 $slideMediaFrm->developerTags['colClassPrefix'] = 'col-md-';
 $slideMediaFrm->developerTags['fld_default_col'] = 12;
 
-$fld1 = $slideMediaFrm->getField('slide_image');	
+$fld1 = $slideMediaFrm->getField('slide_image');
 $fld1->addFieldTagAttribute('class','btn btn--primary btn--sm');
 $screenFld = $slideMediaFrm->getField('slide_screen');
 $screenFld->addFieldTagAttribute('class', 'prefDimensions-js');
 $langFld = $slideMediaFrm->getField('lang_id');
 $langFld->addFieldTagAttribute('class', 'language-js');
-$htmlAfterField = '<div style="margin-top:15px;" class="preferredDimensions-js">'. sprintf(Label::getLabel('LBL_Preferred_Dimensions_%s',$adminLangId),'1920 x 550').'</div>';
+$htmlAfterField = '<div style="margin-top:15px;" class="preferredDimensions-js">'. sprintf(Label::getLabel('LBL_Preferred_Dimensions_%s',$adminLangId),'1920 x 800').'</div>';
 $htmlAfterField .= '<div id="image-listing"></div>';
 $fld1->htmlAfterField = $htmlAfterField;
 ?>
 
 <section class="section">
 	<div class="sectionhead">
-		
+
 		<h4><?php echo Label::getLabel('LBL_Slide_Image_Setup',$adminLangId); ?></h4>
 	</div>
 	<div class="sectionbody space">
-		<div class="row">	
+		<div class="row">
 
 			<div class="col-sm-12">
 				<h1><?php //echo Label::getLabel('LBL_Slide_Image_Setup',$adminLangId); ?></h1>
 				<div class="tabs_nav_container responsive flat">
 					<ul class="tabs_nav">
 						<li><a href="javascript:void(0);" onclick="slideForm(<?php echo $slide_id ?>);"><?php echo Label::getLabel('LBL_General',$adminLangId); ?></a></li>
-						<?php 
+						<?php
 						if ($slide_id > 0) {
 							foreach($languages as $langId=>$langName){?>
 							<li><a href="javascript:void(0);" onclick="slideLangForm(<?php echo $slide_id ?>, <?php echo $langId;?>);"><?php echo Label::getLabel('LBL_'.$langName,$adminLangId);?></a></li>
@@ -41,20 +41,20 @@ $fld1->htmlAfterField = $htmlAfterField;
 							<?php echo $slideMediaFrm->getFormHtml(); ?>
 						</div>
 					</div>
-				</div>	
+				</div>
 			</div>
 		</div>
 	</div>
 </section>
 <script>
 	$(document).on('change','.prefDimensions-js',function(){
-		
+
 		var screenDesktop = <?php echo applicationConstants::SCREEN_DESKTOP ?>;
 		var screenIpad = <?php echo applicationConstants::SCREEN_IPAD ?>;
 
 		if($(this).val() == screenDesktop)
 		{
-			$('.preferredDimensions-js').html((langLbl.preferredDimensions).replace(/%s/g, '1920 x 550'));
+			$('.preferredDimensions-js').html((langLbl.preferredDimensions).replace(/%s/g, '1920 x 800'));
 		}
 		else if($(this).val() == screenIpad)
 		{
