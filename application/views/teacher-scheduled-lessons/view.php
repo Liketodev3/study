5efc615de4fec6ffb7cd7bea239b1ed51fe379fa
ablyabhishek <abhishek.rana@fatbit.in>
2020-07-27 11:32:15 +0530

    task-75552-add-link-on-google-calnder-change-css

--- application/views/teacher-scheduled-lessons/view.php
+++ application/views/teacher-scheduled-lessons/view.php
@@ -1,6 +1,7 @@
 <?php defined('SYSTEM_INIT') or die('Invalid Usage.'); ?>
 <script type="text/javascript">
 lessonId = '<?php echo $lessonId; ?>';
+var canStartAlertLabel = '<?php echo Label::getLabel('LBL_Cannot_Start_The_lesson_Now!'); ?>';
 var is_grpcls = '<?php echo $lessonRow['slesson_grpcls_id']>0 ?>';
 </script>
 <div id="listItems"></div>
