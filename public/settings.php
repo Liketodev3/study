374829d129d018997664094ffc1ade563d416829
ablyrahul <rahul.mittal@fatbit.in>
2020-07-31 16:38:52 +0530

    removed settings file

--- public/settings.php
+++
@@ -1,9 +0,0 @@
-<?php
-// DB
-define('CONF_WEBROOT_FRONTEND', '/');
-define('CONF_WEBROOT_BACKEND', '/admin/');
-define('CONF_DB_SERVER', 'localhost');
-define('CONF_DB_USER', 'root');
-define('CONF_DB_PASS', '');
-define('CONF_DB_NAME', 'test_yocoach');
- ?>
