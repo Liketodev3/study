0223784d6d905c693b0ffd7973f71c2d5754de74
Prinka <prinka.rani@maxixx.com>
2020-09-23 18:32:14 +0530

    resolved cherry pick Conflict

--- admin-application/controllers/TwocheckoutSettingsController.php
+++ admin-application/controllers/TwocheckoutSettingsController.php
@@ -22,18 +22,15 @@ class TwocheckoutSettingsController extends PaymentSettingsController
     private function getForm()
     {
         $frm = new Form('frmPaymentMethods');
-        $paymentTypesArr = array(
-            'HOSTED' => 'Hosted Checkout',
-            'API' => 'Payment API'
-        );
-        $frm->addRadioButtons(Label::getLabel('LBL_Payment_Type', $this->adminLangId), 'payment_type', $paymentTypesArr, 'HOSTED', array('class'=>'box--scroller'));
+
+        $frm->addHiddenField('', 'payment_type', 'API');
         $frm->addRequiredField(Label::getLabel('LBL_Seller_ID', $this->adminLangId), 'sellerId');
         $frm->addRequiredField(Label::getLabel('LBL_Publishable_Key', $this->adminLangId), 'publishableKey');
         $frm->addRequiredField(Label::getLabel('LBL_Private_Key', $this->adminLangId), 'privateKey');
         $frm->addRequiredField(Label::getLabel('LBL_Secret_Word', $this->adminLangId), 'hashSecretWord');
 
-        $frm->addHTML('Remember', ' ', 'In case of <strong>Hosted Checkout</strong>, Admin must set <strong>Direct Return (URL)</strong> to <strong>Header Redirect</strong> and
-		<strong>Approved URL</strong> to <strong>'.CommonHelper::generateFullUrl('twocheckout_pay', 'callback', array(), CONF_WEBROOT_URL).'</strong> under <strong>2Checkout Accounts</strong> Section.<br/><br/>');
+        /* $frm->addHTML('Remember', ' ', 'In case of <strong>Hosted Checkout</strong>, Admin must set <strong>Direct Return (URL)</strong> to <strong>Header Redirect</strong> and
+		<strong>Approved URL</strong> to <strong>'.CommonHelper::generateFullUrl('twocheckout_pay', 'callback', array(), CONF_WEBROOT_URL).'</strong> under <strong>2Checkout Accounts</strong> Section.<br/><br/>'); */
         $frm->addSubmitButton('', 'btn_submit', Label::getLabel('LBL_Save_Changes', $this->adminLangId));
         return $frm;
     }
--- database/db-updates.sql
+++ database/db-updates.sql
@@ -1,1 +1,2 @@
-REPLACE INTO `tbl_payment_methods` (`pmethod_identifier`, `pmethod_code`, `pmethod_active`, `pmethod_display_order`,`pmethod_type`) VALUES ('2Checkout', '2Checkout', '1', 6, 1);
+REPLACE INTO `tbl_payment_methods` (`pmethod_identifier`, `pmethod_code`, `pmethod_active`, `pmethod_display_order`,`pmethod_type`) VALUES ('Twocheckout', 'Twocheckout', '1', 6, 1);
+
