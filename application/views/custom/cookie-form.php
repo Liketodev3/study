836d8ddacd0f62579205eed158a01a7350b99ded
ablyabhishek <abhishek.rana@fatbit.in>
2020-10-16 13:27:07 +0530

    task-77712-changed cookie consent popup html and css

--- application/views/custom/cookie-form.php
+++ application/views/custom/cookie-form.php
@@ -5,19 +5,27 @@ $cookieForm->developerTags['colClassPrefix'] = 'col-md-';
 $cookieForm->developerTags['fld_default_col'] = 7;
 $cookieForm->setFormTagAttribute('autocomplete', 'off');
 $cookieForm->setFormTagAttribute('onsubmit', 'saveCookieSetting(this); return false;');
+$necessary = $cookieForm->getField(UserCookieConsent::COOKIE_NECESSARY_FIELD);
+$necessary->addFieldTagAttribute('disabled',true);
+
 ?>
 
 <div class="tab-container cookie-consent">
-
-	<div class="tabs-small tabs-offset tabs-scroll-js">
-		<ul>
-			<li><a href="#" class="tab-a is-active" data-id="tab_necessary"><?php echo Label::getLabel('LBL_Necessary'); ?></a></li>
-			<li><a href="#" class="tab-a" data-id="tab_preferences"><?php echo Label::getLabel('LBL_Preferences'); ?></a></li>
-			<li><a href="#" class="tab-a" data-id="tab_statistics"><?php echo Label::getLabel('LBL_Statistics'); ?></a></li>
-		</ul>
+    <div class="coookie-popup-head">
+	  <div class="popup-heading">
+	     <h3><?php echo Label::getLabel('LBL_COOKIE_CONSENT_HEADING'); ?></h3>
+	  </div>
 	</div>
-	<!--end of tab-menu-->
-	<?php echo $cookieForm->getFormTag(); ?>
+	<div class="coookie-popup-body">
+		<div class="tabs-small tabs-offset tabs-scroll-js">
+			<ul>
+				<li><a href="#" class="tab-a is-active" data-id="tab_necessary"><?php echo Label::getLabel('LBL_Necessary'); ?></a></li>
+				<li><a href="#" class="tab-a" data-id="tab_preferences"><?php echo Label::getLabel('LBL_Preferences'); ?></a></li>
+				<li><a href="#" class="tab-a" data-id="tab_statistics"><?php echo Label::getLabel('LBL_Statistics'); ?></a></li>
+			</ul>
+		</div>
+
+		<?php echo $cookieForm->getFormTag(); ?>
 	<div class="tab tab-active" data-id="tab_necessary">
 	    <div class="tab-heading">
 		 	<h2><?php echo Label::getLabel('LBL_Necessary'); ?></h2>
@@ -45,20 +53,24 @@ $cookieForm->setFormTagAttribute('onsubmit', 'saveCookieSetting(this); return fa
 		</div>
 		<p><?php echo Label::getLabel('LBL_STATISTICS_COOKIE_DESCRIPTION_TEXT'); ?></p>
 	</div>
+	</form>
+	</div>
+	<!--end of tab-menu-->
 
 	<div class="coookie-popup-footer">
 		<?php echo $cookieForm->getFieldHtml('btn_submit'); ?>
 	</div>
-	</form>
 </div>
 <?php echo $cookieForm->getExternalJS(); ?>
 <!--end of container-->
 <script>
+	var necessaryField = '<?php echo UserCookieConsent::COOKIE_NECESSARY_FIELD ?>';
 	$(document).ready(function() {
 		
 		$('.cookie-consent .field_cover label').each(function( index ) {
-					let inputHtml = $(this).html()
-				$(this).html('<span class="checkbox">'+inputHtml+'<i class="input-helper"></i></span>');
+			let inputHtml = $(this).html()
+			spanClass = ($(this).find('input').attr('name') == necessaryField) ? 'disabled' : '';
+			$(this).html('<span class="checkbox '+spanClass+'">'+inputHtml+'<i class="input-helper"></i></span>');
 		});
 
 		$('.tab-a').click(function() {
