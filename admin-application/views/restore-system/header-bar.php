2d6fb7a081b121b6e622579644844b086fea4111
Prinka <prinka.rani@maxixx.com>
2020-11-24 16:39:18 +0530

    task-76258-fix restore timing issues

--- admin-application/views/restore-system/header-bar.php
+++ admin-application/views/restore-system/header-bar.php
@@ -476,17 +476,19 @@ $restoreTime = FatApp::getConfig('CONF_RESTORE_SCHEDULE_TIME', FatUtility::VAR_S
         $('html').removeClass('sticky-demo-header');
     });
     // Set the date we're counting down to
-    var countDownDate = new Date('2020-10-16 20:13:44').getTime();
+    var countDownDate = new Date('<?php echo $restoreTime; ?>').getTime();
 
     // Update the count down every 1 second
     var x = setInterval(function() {
 
         // Get today's date and time
         //var now = new Date().getTime();
-        var currentTime = new Date();
-        var currentOffset = currentTime.getTimezoneOffset();
-        var ISTOffset = 330; // IST offset UTC +5:30
-        var now = new Date(currentTime.getTime() + (ISTOffset + currentOffset) * 60000);
+        var date = new Date();
+        var utcDate = new Date(date.toLocaleString('en-US', {
+            timeZone: "UTC"
+        }));
+
+        var now = utcDate.getTime();
 
         // Find the distance between now and the count down date
         var distance = countDownDate - now;
@@ -500,13 +502,13 @@ $restoreTime = FatApp::getConfig('CONF_RESTORE_SCHEDULE_TIME', FatUtility::VAR_S
         var str = ('0' + hours).slice(-2) + ":" + ('0' + minutes).slice(-2) + ":" + ('0' + seconds).slice(-2);
         // Display the result in the element with id="demo"
         document.getElementById("restoreCounter").innerHTML = str;
-        //$('#restoreCounter').html(str);
+
         var progressPercentage = 100 - (parseFloat(hours + '.' + parseFloat(minutes / 15 * 25)) * 100 / 4);
         $('.restore__progress-bar').css('width', progressPercentage + '%');
         // If the count down is finished, write some text
         if (distance < 0) {
             clearInterval(x);
-            $('#restoreCounter').html("Process...");
+            document.getElementById("restoreCounter").innerHTML = "Process...";
             showRestorePopup();
             restoreSystem();
         }
