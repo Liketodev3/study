686561c096d6b2e11e9cfcbf7e704dd8039c6d90
ablyrahul <rahul.mittal@fatbit.in>
2020-12-23 16:16:11 +0530

    task-78843-upgraded zoom version

--- application/views/zoom/meeting.php
+++ application/views/zoom/meeting.php
@@ -1,13 +1,12 @@
 <?php
 defined('SYSTEM_INIT') or die('Invalid Usage.');
 ?>
-<script src="https://source.zoom.us/1.8.3/lib/vendor/react.min.js"></script>
-<script src="https://source.zoom.us/1.8.3/lib/vendor/react-dom.min.js"></script>
-<script src="https://source.zoom.us/1.8.3/lib/vendor/redux.min.js"></script>
-<script src="https://source.zoom.us/1.8.3/lib/vendor/redux-thunk.min.js"></script>
-<script src="https://source.zoom.us/1.8.3/lib/vendor/jquery.min.js"></script>
-<script src="https://source.zoom.us/1.8.3/lib/vendor/lodash.min.js"></script>
-<script src="https://source.zoom.us/zoom-meeting-1.8.3.min.js"></script>
+<script src="https://source.zoom.us/<?php echo CONF_ZOOM_VERSION ?>/lib/vendor/react.min.js"></script>
+<script src="https://source.zoom.us/<?php echo CONF_ZOOM_VERSION ?>/lib/vendor/react-dom.min.js"></script>
+<script src="https://source.zoom.us/<?php echo CONF_ZOOM_VERSION ?>/lib/vendor/redux.min.js"></script>
+<script src="https://source.zoom.us/<?php echo CONF_ZOOM_VERSION ?>/lib/vendor/redux-thunk.min.js"></script>
+<script src="https://source.zoom.us/<?php echo CONF_ZOOM_VERSION ?>/lib/vendor/lodash.min.js"></script>
+<script src="https://source.zoom.us/zoom-meeting-<?php echo CONF_ZOOM_VERSION ?>.min.js"></script>
 <script src="<?php echo CONF_WEBROOT_URL ?>js/zoom_tool.js"></script>
 <script src="<?php echo CONF_WEBROOT_URL ?>js/vconsole.min.js"></script>
 <script src="<?php echo CONF_WEBROOT_URL ?>js/meeting.js"></script>
