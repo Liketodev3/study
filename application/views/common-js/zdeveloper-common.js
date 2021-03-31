6aa3ca1b34cf37874d69c2b50bf27edaff4920f5
Neetu Arora <neetu.rani@fatbit.in>
2021-03-31 13:44:34 +0530

    bug-051663-change css class and js method

--- application/views/common-js/zdeveloper-common.js
+++ application/views/common-js/zdeveloper-common.js
@@ -415,12 +415,11 @@ $(document).ready(function () {
 	};
 
 	toggleTeacherFavorite = function (teacher_id, el) {
+		
 		if (isRuningTeacherFavoriteAjax) {
 			return false;
 		}
-
 		isRuningTeacherFavoriteAjax = true;
-
 		if (isUserLogged() == 0) {
 			logInFormPopUp();
 			return false;
@@ -430,10 +429,11 @@ $(document).ready(function () {
 		fcom.updateWithAjax(fcom.makeUrl('Learner', 'toggleTeacherFavorite'), data, function (ans) {
 			isRuningTeacherFavoriteAjax = false;
 			if (ans.status) {
+				
 				if (ans.action == 'A') {
 					$(el).addClass("is-active");
 				} else if (ans.action == 'R') {
-					$(el).removeClass("is-active");
+					$(el).removeClass("is-active");	
 				}
 				if (typeof searchfavorites != 'undefined') {
 					searchfavorites(document.frmFavSrch);
