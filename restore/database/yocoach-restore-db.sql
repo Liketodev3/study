6335f91a73a2bf4d96651a8e096fc9551a3dfcbf
Prinka <prinka.rani@maxixx.com>
2021-04-08 16:48:10 +0530

    update db files

--- restore/database/yocoach-restore-db.sql
+++ restore/database/yocoach-restore-db.sql
@@ -11395,11 +11395,9 @@ REPLACE INTO `tbl_language_labels` (`label_key`, `label_lang_id`, `label_caption
 
 ALTER TABLE `tbl_meta_tags` ADD `meta_type` TINYINT(1) NOT NULL AFTER `meta_action`;
 
-ALTER TABLE `tbl_meta_tags` CHANGE `meta_record_id` `meta_record_id` VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL;
+ALTER TABLE `tbl_meta_tags` CHANGE `meta_record_id` `meta_record_id` VARCHAR(255) NOT NULL;
 
-INSERT INTO `tbl_meta_tags` (`meta_id`, `meta_controller`, `meta_action`, `meta_type`, `meta_record_id`, `meta_subrecord_id`, `meta_identifier`, `meta_default`, `meta_advanced`) VALUES (1, '', '', '-1', '0', '0', 'Yo!Coach Live Demo', '0', '0');
-
-INSERT INTO `tbl_meta_tags_lang` (`metalang_meta_id`, `metalang_lang_id`, `meta_title`, `meta_keywords`, `meta_description`, `meta_other_meta_tags`) VALUES ('1', '1', 'Yo!Coach Live Demo - Readymade Solution To Build Online Learning & Consultation Marketplace | Yo!Coach', '', 'Check the working and features of Yo!Coach with live demo setup. A FATbit Technologies solution to build an online learning and consultation platform like Verbling , Italki, Preply, and Cambly. ', '');
+REPLACE INTO `tbl_meta_tags` ( `meta_controller`, `meta_action`, `meta_type`, `meta_record_id`, `meta_subrecord_id`, `meta_identifier`, `meta_default`, `meta_advanced`) VALUES ('', '', '-1', '0', '0', 'Yo!Coach Live Demo', '0', '0');
 
 UPDATE `tbl_configurations` SET `conf_val` = 'TV-2.11.0.20210317' WHERE `tbl_configurations`.`conf_name` = 'CONF_YOCOACH_VERSION'; 
 
