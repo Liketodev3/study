c3920ffbf617363f414812356e4b96f8014acb63
ablyabhishek <abhishek.rana@fatbit.in>
2021-05-03 13:31:38 +0530

    task-83884-add end lesson label

--- application/views/header/top.php
+++ application/views/header/top.php
@@ -46,7 +46,7 @@ if(FatApp::getConfig('conf_auto_restore_on', FatUtility::VAR_INT, 1) && CommonHe
 								$unreadNotificationsCnt = CommonHelper::getUnreadNotifications();
 							?>
                             <li>
-                                <a href="<?php echo CommonHelper::generateUrl('Messages'); ?>" class="nav__dropdown-action nav__dropdown-msg">
+                                <a href="<?php echo CommonHelper::generateUrl('Messages','', [], CONF_WEBROOT_DASHBOARD); ?>" class="nav__dropdown-action nav__dropdown-msg">
                                     <span class="svg-icon"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 18">
                                             <path d="M1219.07,134h-18.14a2.918,2.918,0,0,0-2.93,2.892v12.216a2.918,2.918,0,0,0,2.93,2.892h18.13a2.92,2.92,0,0,0,2.94-2.892V136.9A2.916,2.916,0,0,0,1219.07,134Zm1.59,15.108a1.58,1.58,0,0,1-1.59,1.571h-18.14a1.587,1.587,0,0,1-1.59-1.571V136.9a1.587,1.587,0,0,1,1.59-1.571h18.13a1.589,1.589,0,0,1,1.6,1.571v12.211h0Zm-7.52-6.26,5.87-5.187a0.658,0.658,0,0,0,.04-0.935,0.67,0.67,0,0,0-.94-0.049l-8.1,7.16-1.58-1.39s-0.01-.01-0.01-0.015a0.728,0.728,0,0,0-.11-0.093l-6.42-5.667a0.678,0.678,0,0,0-.95.054,0.659,0.659,0,0,0,.05.935l5.94,5.231-5.91,5.457a0.658,0.658,0,0,0-.03.935,0.678,0.678,0,0,0,.49.21,0.691,0.691,0,0,0,.46-0.176l6-5.535,1.63,1.434a0.68,0.68,0,0,0,.45.166,0.642,0.642,0,0,0,.44-0.171l1.68-1.478,5.97,5.589a0.659,0.659,0,0,0,.46.181,0.662,0.662,0,0,0,.46-1.14Z" transform="translate(-1198 -134)" />
                                         </svg></span><span class="unrdMsgCnt">
@@ -54,7 +54,7 @@ if(FatApp::getConfig('conf_auto_restore_on', FatUtility::VAR_INT, 1) && CommonHe
                                 </a>
                             </li>
                             <li class="nav__dropdown nav__dropdown--notification">
-                                <a href="<?php echo CommonHelper::generateUrl('Notifications'); ?>" class="nav__dropdown-action nav__dropdown-msg nav__dropdown-trigger-js">
+                                <a href="<?php echo CommonHelper::generateUrl('Notifications', '', [], CONF_WEBROOT_DASHBOARD); ?>" class="nav__dropdown-action nav__dropdown-msg nav__dropdown-trigger-js">
                                     <span class="svg-icon"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 21 22">
                                             <path d="M1294,152.24h-21v-0.462a4,4,0,0,1,3.09-3.88v-5.379a7.41,7.41,0,1,1,14.82,0V147.9a4,4,0,0,1,3.09,3.88v0.462Zm-20.05-.919h19.1a3.09,3.09,0,0,0-2.66-2.581l-0.4-.049v-6.172a6.49,6.49,0,1,0-12.98,0v6.172l-0.4.049A3.09,3.09,0,0,0,1273.95,151.321Zm8.27-15.567h-0.91V135.2a2.19,2.19,0,1,1,4.38,0v0.541h-0.91V135.2a1.28,1.28,0,1,0-2.56,0v0.55ZM1283.49,155a3.825,3.825,0,0,1-3.73-3.061l0.9-.184a2.89,2.89,0,0,0,5.68-.057l0.9,0.166A3.815,3.815,0,0,1,1283.49,155Zm-6.94-7.176h1.68v0.92h-1.68v-0.92Zm4.32,0h9.58v0.92h-9.58v-0.92Z" transform="translate(-1273 -133)" />
                                         </svg></span>
@@ -94,14 +94,14 @@ if(FatApp::getConfig('conf_auto_restore_on', FatUtility::VAR_INT, 1) && CommonHe
                                                         <h6><?php echo $notifications['noti_title']; ?></h6>
                                                         <p><?php echo $notifications['noti_desc']; ?></p>
                                                     </div>
-                                                    <a href="<?php echo CommonHelper::generateUrl('notifications','readNotification',array($notifications['noti_id'])); ?>" class="list__action"></a>
+                                                    <a href="<?php echo CommonHelper::generateUrl('notifications','readNotification',array($notifications['noti_id']), CONF_WEBROOT_DASHBOARD); ?>" class="list__action"></a>
                                                 </div>
                                                 <?php } ?>
                                             </div>
 
                                         </div>
                                         <div class="list-container__footer">
-                                            <a href="<?php echo CommonHelper::generateUrl('Notifications'); ?>" class="btn btn--secondary btn--block"><?php echo Label::getLabel("LBL_View_all"); ?></a>
+                                            <a href="<?php echo CommonHelper::generateUrl('Notifications','', [], CONF_WEBROOT_DASHBOARD); ?>" class="btn btn--secondary btn--block"><?php echo Label::getLabel("LBL_View_all"); ?></a>
                                         </div>
                                     </div>
                                 </div>
--- dashboard-application/controllers/LearnerController.php
+++ dashboard-application/controllers/LearnerController.php
@@ -160,9 +160,9 @@ class LearnerController extends LearnerBaseController
         $srch->addMultipleFields(['user_id', 'user_first_name', 'user_last_name',]);
         $teacher = FatApp::getDb()->fetch($srch->getResultSet());
         if (empty($teacher)) {
-            Message::addErrorMessage(Label::getLabel('LBL_Invalid_Request', $this->siteLangId));
-            FatUtility::dieWithError(Message::getHtml());
+            FatUtility::dieJsonError(Label::getLabel('LBL_Invalid_Request', $this->siteLangId));
         }
+        $message = '';
         $action = 'N'; //nothing happened
         $srch = new UserFavoriteTeacherSearch();
         $srch->doNotCalculateRecords();
@@ -172,21 +172,18 @@ class LearnerController extends LearnerBaseController
         if (!$row = $db->fetch($srch->getResultSet())) {
             $userObj = new User($loggedUserId);
             if (!$userObj->addUpdateUserFavoriteTeacher($teacherId)) {
-                Message::addErrorMessage(Label::getLabel('LBL_Some_problem_occurred,_Please_contact_webmaster', $this->siteLangId));
-                FatUtility::dieWithError(Message::getHtml());
+                $message = Label::getLabel('LBL_Some_problem_occurred,_Please_contact_webmaster', $this->siteLangId);
             }
             $action = 'A'; //Added to favorite
-            $this->set('msg', Label::getLabel('LBL_Teacher_has_been_marked_as_favourite_successfully', $this->siteLangId));
+            $message = Label::getLabel('LBL_Teacher_has_been_marked_as_favourite_successfully', $this->siteLangId);
         } else {
             if (!$db->deleteRecords(User::DB_TBL_TEACHER_FAVORITE, ['smt' => 'uft_user_id = ? AND uft_teacher_id = ?', 'vals' => [$loggedUserId, $teacherId]])) {
-                Message::addErrorMessage(Label::getLabel('LBL_Some_problem_occurred,_Please_contact_webmaster', $this->siteLangId));
-                FatUtility::dieWithError(Message::getHtml());
+                $message = Label::getLabel('LBL_Some_problem_occurred,_Please_contact_webmaster', $this->siteLangId);
             }
             $action = 'R'; //Removed from favorite
-            $this->set('msg', Label::getLabel('LBL_Teacher_has_been_removed_from_favourite_list', $this->siteLangId));
+            $message = Label::getLabel('LBL_Teacher_has_been_removed_from_favourite_list', $this->siteLangId);
         }
-        $this->set('action', $action);
-        $this->_template->render(false, false, 'json-success.php');
+        FatUtility::dieJsonSuccess(['msg' => $message, 'action' => $action]);
     }
 
     public function favourites()
