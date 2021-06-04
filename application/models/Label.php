4a204529f0d4dc5169aebaf97dbeda131865b2e9
Prinka <prinka.rani@maxixx.com>
2021-06-04 14:58:34 +0530

    bug-054442,054443,054638-fix some rtl issues- resolve conflicts in cherry pick

--- application/models/Label.php
+++ application/models/Label.php
@@ -11,14 +11,6 @@ class Label extends MyAppModel
         parent::__construct(static::DB_TBL, static::DB_TBL_PREFIX . 'id', $labelId);
     }
 
-    public static function getInstance()
-    {
-        if (self::$_instance === null) {
-            self::$_instance = new self();
-        }
-        return self::$_instance;
-    }
-
     public static function getSearchObject($langId = 0)
     {
         $langId = FatUtility::int($langId);
