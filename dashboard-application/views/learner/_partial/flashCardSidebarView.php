1776cc9bb122f8c975dbb7936e7cc9356208b9b5
ablyabhishek <abhishek.rana@fatbit.in>
2021-06-18 15:44:05 +0530

    task-055150-fix search flash card on blur

--- dashboard-application/views/learner/_partial/flashCardSidebarView.php
+++ dashboard-application/views/learner/_partial/flashCardSidebarView.php
@@ -14,7 +14,7 @@
 		<a href="javascript:void(0);" onclick="flashCardForm(<?php echo $lessonRow['slesson_id'] ?>, 0, <?php echo $lessonRow['learnerId'] ?>, <?php echo $lessonRow['teacherId'] ?>);" class="color-secondary underline padding-top-3 padding-bottom-3 flash-card-add-js">  <?php echo Label::getLabel('LBL_Add'); ?></a>
 	</div>
 	<div class="fcard-search__body">    
-		<input type="text" placeholder="Search flashcard..">
+	<?php echo $frmSrchFlashCard->getFieldHtml('keyword'); ?>
 		<span class="form__action-wrap">
 			<span class="svg-icon">
 				<svg><use xlink:href="<?php echo CONF_WEBROOT_URL.'images/sprite.yo-coach.svg#search'; ?>"></use></svg>
