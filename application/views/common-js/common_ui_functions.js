a05edc3dafd02080b759a4edd58ab599da0910ab
Sandeep Arora <sandeep.arora@fatbit.in>
2021-06-18 13:04:02 +0530

    task-86406-fix Ui bugs

--- application/views/common-js/common_ui_functions.js
+++ application/views/common-js/common_ui_functions.js
@@ -104,7 +104,7 @@ $(document).ready(function() {
     
     $(".toggle--nav-js").click(function () {
       $(this).toggleClass("is-active");
-      $('html').toggleClass("show-nav-js");
+      $('html').addClass("show-nav-js");
       $('html').removeClass("show-dashboard-js");
     });
 
--- application/views/header/top.php
+++ application/views/header/top.php
@@ -12,7 +12,7 @@
         <div class="container">
             <div class="d-flex justify-content-between align-items-center">
                 <div class="header__left">
-                    <a href="#" class="toggle toggle--nav toggle--nav-js">
+                    <a href="javascript:void(0)" class="toggle toggle--nav toggle--nav-js">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 515.555 515.555"><path d="m303.347 18.875c25.167 25.167 25.167 65.971 0 91.138s-65.971 25.167-91.138 0-25.167-65.971 0-91.138c25.166-25.167 65.97-25.167 91.138 0"/><path d="m303.347 212.209c25.167 25.167 25.167 65.971 0 91.138s-65.971 25.167-91.138 0-25.167-65.971 0-91.138c25.166-25.167 65.97-25.167 91.138 0"/><path d="m303.347 405.541c25.167 25.167 25.167 65.971 0 91.138s-65.971 25.167-91.138 0-25.167-65.971 0-91.138c25.166-25.167 65.97-25.167 91.138 0"/></svg>
                     </a>
                     <div class="header__logo">
