ecc122c211d8aebb473d4e00d62333b4d20858da
ablyabhishek <abhishek.rana@fatbit.in>
2021-06-21 18:20:58 +0530

    bug-053426-change in update email view page

--- application/views/guest-user/configure-email.php
+++ application/views/guest-user/configure-email.php
@@ -20,7 +20,7 @@ $fld->developerTags['col'] = 12;
 	<div class="container container--fixed">
 		<div class="row justify-content-center">
 			<div class="col-sm-9 col-lg-5 col-xl-5">
-				Please contact wemaster <a href="mailto:<?php echo FatApp::getConfig('conf_site_owner_email') ?>"><?php echo FatApp::getConfig('conf_site_owner_email') ?></a>
+				<?php echo Label::getLabel('LBL_PLEASE_CONTACT_WEMASTER'); ?> <a href="mailto:<?php echo FatApp::getConfig('conf_site_owner_email') ?>"><?php echo FatApp::getConfig('conf_site_owner_email') ?></a>
 			</div>
 		</div>
 	</div>
