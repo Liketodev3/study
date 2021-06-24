ae179dc5af8610d12ff692951c9a87ad8fbedda8
Sher Singh <sher.singh@fatbit.in>
2021-06-24 09:45:49 +0530

    Task-86405 added close and removed logout on teacher request screens

--- application/views/teacher-request/_partial/header.php
+++ application/views/teacher-request/_partial/header.php
@@ -1,37 +1,32 @@
 <?php defined('SYSTEM_INIT') or die('Invalid Usage.'); ?>
-
-
-	<header class="header">
-		<div class="container container--narrow">
-			<div class="header-primary">
-				<div class="d-flex justify-content-between">
-					<div class="header__left">
-						<div class="header__logo">
-						<a href="<?php echo CommonHelper::generateUrl(); ?>">
+<header class="header">
+    <div class="container container--narrow">
+        <div class="header-primary">
+            <div class="d-flex justify-content-between">
+                <div class="header__left">
+                    <div class="header__logo">
+                        <a href="<?php echo CommonHelper::generateUrl(); ?>">
                             <img src="<?php echo CommonHelper::generateFullUrl('Image', 'siteLogo', array($siteLangId), CONF_WEBROOT_FRONT_URL); ?>" alt="">
                         </a>
-						</div>
-					</div>
+                    </div>
+                </div>
+                <div class="header__right">
+                    <div class="head__action">
+                        <a class="" href="<?php echo (UserAuthentication::getLoggedUserId(true)) ? CommonHelper::generateUrl('GuestUser', 'logout') : CommonHelper::generateUrl('TeacherRequest', 'logoutGuestUser'); ?>">
+                            <svg class="icon icon--logout">
+                            <use xlink:href="<?php echo CONF_WEBROOT_URL . 'images/sprite.yo-coach.svg#close' ?>"></use>
+                            </svg>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
 
-					<div class="header__right">
 
-						<div class="head__action">
-							<a class="" href="<?php echo (UserAuthentication::getLoggedUserId(true))?CommonHelper::generateUrl('GuestUser','logout'):CommonHelper::generateUrl('TeacherRequest','logoutGuestUser'); ?>">
-								<svg class="icon icon--logout">
-									<use xlink:href="<?php echo CONF_WEBROOT_URL.'images/sprite.yo-coach.svg#logout' ?>"></use>
-								</svg>
-								<span><?php echo Label::getLabel('LBL_Logout',$siteLangId); ?></span>
-							</a>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</header>
 
-	
-
-	<!-- ] -->
+<!-- ] -->
 
 
 
