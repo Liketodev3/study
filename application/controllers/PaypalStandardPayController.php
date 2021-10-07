7a9368fcb96436b06676330b8dc235d2bea89035
Liketodev2 <liketodev2@gmail.com>
2021-10-07 18:33:31 +0400

    paypal standard changes back

--- application/controllers/PaypalStandardPayController.php
+++ application/controllers/PaypalStandardPayController.php
@@ -82,6 +82,7 @@ class PaypalStandardPayController extends PaymentController
 
     public function charge($orderId)
     {
+
         if ($orderId == '') {
             Message::addErrorMessage(Label::getLabel('MSG_Invalid_Access', $this->siteLangId));
             CommonHelper::redirectUserReferer();
@@ -273,12 +274,11 @@ class PaypalStandardPayController extends PaymentController
             if (!$totalPaidMatch) {
                 $request .= "\n\n PP_STANDARD :: TOTAL PAID MISMATCH! " . strtolower($post['mc_gross']) . "\n\n";
             }
-    /*        if ($orderPaymentStatus == Order::ORDER_IS_PAID && $receiverMatch && $totalPaidMatch) {
+               if ($orderPaymentStatus == Order::ORDER_IS_PAID && $receiverMatch && $totalPaidMatch) {
                 $orderPaymentObj->addOrderPayment($paymentSettings["pmethod_code"], $post["txn_id"], $paymentGatewayCharge, 'Received Payment', $request . "#" . $response);
             } else {
                 $orderPaymentObj->addOrderPaymentComments($request);
-            }*/
-            $orderPaymentObj->addOrderPayment($paymentSettings["pmethod_code"], $post["txn_id"], $paymentGatewayCharge, 'Received Payment', $request . "#" . $response);
+            }
         }
         curl_close($curl);
     }
